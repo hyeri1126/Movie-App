64ee444fb62e5887119854663cfd6a9e6e89222a
hyeri1126 <hyeri1126@ewhain.net>
2023-06-08 21:26:39 +0900

    리팩토링 : 전체 영역을 감싸는 ScrollView -> FlatList로 변경

--- screens/Movies.js
+++ screens/Movies.js
@@ -11,7 +11,7 @@ import HMedia from '../components/HorizontalMedia';
 
 
 
-const Container = styled.ScrollView`
+const Container = styled.FlatList`
     background-color: ${props => props.theme.mainBgColor};
 `;
 
@@ -84,63 +84,59 @@ const Movies = ({navigation:{navigate}}) => {
         </Loader>
     ) : (
       <Container
-        refreshControl={
-            <RefreshControl 
-                refreshing={refreshing}
-                onRefresh={onRefresh} />
-        }>
-        <Swiper 
-            loop 
-            timeout={3} 
-            controlsEnabled={false}
-            containerStyle={{ marginBottom:20, width:"100%", height: SCREEN_HEIGHT/4 }}
-        >
-           {nowPlaying.map(movie => (
-            <Slide key={movie.id}
-                background_image={movie.background_image}
-                medium_cover_image={movie.medium_cover_image}
-                title={movie.title}
-                rating={movie.rating}
-                summary={movie.summary}
-            />
-           ))}
-        </Swiper>
-        <ListContainer>
-            <ListTitle>Trending Movies</ListTitle>
-            <FlatList 
-                data={trending}
-                horizontal
-                keyExtractor={(item)=>item.id + ""}
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{paddingHorizontal:13}}
-                ItemSeparatorComponent={() => 
-                    <View style={{width:13}} />
-                }
-                renderItem={({item}) => 
-                    <VMedia 
-                        posterImage={item.medium_cover_image}
-                        title={item.title}
-                        rating={item.rating}
-                    />
-                }
-            />
-        </ListContainer>
-        <CommingSoonTitle>Comming Soon</CommingSoonTitle>
-        <FlatList 
-            data={upComing}
-            keyExtractor={(item)=>item.id+""}
-            ItemSeparatorComponent={() => <View style={{height:30}} />}
-            renderItem={({item}) => 
-                <HMedia 
-                    posterPath={item.medium_cover_image}
-                    title={item.title}
-                    summary={item.summary}
-                    uploadData={item.date_uploaded}
-                    rating={item.rating}
+        ListHeaderComponent={<>
+            <Swiper 
+                loop 
+                timeout={3} 
+                controlsEnabled={false}
+                containerStyle={{ marginBottom:20, width:"100%", height: SCREEN_HEIGHT/4 }}
+            >
+            {nowPlaying.map(movie => (
+                <Slide key={movie.id}
+                    background_image={movie.background_image}
+                    medium_cover_image={movie.medium_cover_image}
+                    title={movie.title}
+                    rating={movie.rating}
+                    summary={movie.summary}
                 />
+            ))}
+            </Swiper>
+            <ListContainer>
+                <ListTitle>Trending Movies</ListTitle>
+                <FlatList 
+                    data={trending}
+                    horizontal
+                    keyExtractor={(item)=>item.id + ""}
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{paddingHorizontal:13}}
+                    ItemSeparatorComponent={() => 
+                        <View style={{width:13}} />
+                    }
+                    renderItem={({item}) => 
+                        <VMedia 
+                            posterImage={item.medium_cover_image}
+                            title={item.title}
+                            rating={item.rating}
+                        />
+                    }
+                />
+            </ListContainer>
+            <CommingSoonTitle>Comming Soon</CommingSoonTitle>
+        </>}
+        data={upComing}
+        keyExtractor={(item)=>item.id+""}
+        ItemSeparatorComponent={() => <View style={{height:30}} />}
+        renderItem={({item}) => 
+            <HMedia 
+                posterPath={item.medium_cover_image}
+                title={item.title}
+                summary={item.summary}
+                uploadData={item.date_uploaded}
+                rating={item.rating}
+            />
         }
-        />
-      </Container>
+
+      />
     )
 }
 
