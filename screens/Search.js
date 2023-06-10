a378aa4acdff2ff19aed61eddc09c95e61b82d32
hyeri1126 <hyeri1126@ewhain.net>
2023-06-11 03:24:43 +0900

    에뮬레이터 실행 안됨

--- screens/Search.js
+++ screens/Search.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import { View, Text } from 'react-native';
 
+const Container = styled.ScrollViw`
+
+`
+const SearchBar = styled.TextInput`
+    background-color: white;
+    padding:10px 15px;
+    border-radius: 15px;
+    width: 90%;
+    margin: 10px auto;
+`
 const Search = () => {
+    const [query,setQuery] = useState("");
+    const onChangeText = (text) => setQuery(text)
+    console.log(query)
     return(
-        <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-            <Text>Search</Text>
-        </View>
+        <Container>
+            <SearchBar 
+                placeholder="Search for Movie or TV Show"
+                placehloderTextColor="grey"
+                returnKeyLabel="search"
+                onChange={onChangeText}
+            />
+        </Container>
     )
 }
 
