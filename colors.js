08107228ac9c4662ac25db9e839528b64d8963ff
hyeri1126 <hyeri1126@ewhain.net>
2023-06-11 20:20:22 +0900

    color 추가

--- colors.js
+++ colors.js
@@ -7,3 +7,4 @@ export const YELLOW_COLOR = "#ffa801";
 export const BLACK_COLOR = "#222f3e";
 export const DARK_GREY = "#d2dae2";
 export const LIGHT_GREY = '#808e9b';
+export const WHITE = "white";
